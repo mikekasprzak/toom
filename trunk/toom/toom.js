89fd158c3e2feae0d5da274749ee69d1b78b3519
povrazor <povrazor@27a7268d-3c14-4fb0-ae7b-d745656660af>
2013-04-27 19:47:18 +0000

    Layering in.

--- trunk/toom/toom.js
+++ trunk/toom/toom.js
@@ -8,22 +8,31 @@ function Step() {
 	
 }
 
+function DrawLayer( layer ) {
+	for ( var idx = 0; idx < layer.length; idx++ ) {
+		imgDraw( Art[layer[idx].img], layer[idx].x, layer[idx].y );
+	}
+
+}
+
 function Draw() {
 	ctx.fillStyle = "#221133";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	
+	DrawLayer( BGLayer );
 	imgDraw( Art.Man, 0, 64, (Stepper>>3)&3 );
+	DrawLayer( FGLayer );
 	
 	
 	ctx.fillStyle = "#FFFFFF";
 	ctx.font = '20px Pixel';
 	var Text = 'Hello Drek';
-	ctx.fillText(Text, BaseX-55, BaseY-64-20);
+	ctx.fillText(Text, BaseX-65, BaseY-48-20);
 
 	Text = "I've been protecting you_";
 	if ( (Stepper >> 5)&1 ) {
 		Text = "I've been protecting you";
 	}
-	ctx.fillText(Text, BaseX-120, BaseY-64);
+	ctx.fillText(Text, BaseX-130, BaseY-48);
 
 }
