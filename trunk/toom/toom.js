0980c8165ed54343b66409cd5ca67f25508ddadd
povrazor <povrazor@27a7268d-3c14-4fb0-ae7b-d745656660af>
2013-04-27 18:27:08 +0000

    Font tweak.

--- trunk/toom/toom.js
+++ trunk/toom/toom.js
@@ -28,7 +28,7 @@ function Draw() {
 	
 	
 	ctx.fillStyle = "#FFFFFF";
-	ctx.font = '20px Pixel';
+	ctx.font = '16px Pixel';
 	var Text = 'Hello Drek';
 	ctx.fillText(Text, (canvas.width>>1)-55, (canvas.height>>1)-96-20);
 
