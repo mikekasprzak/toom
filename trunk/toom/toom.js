a0d44d9b9de2df96566f74d24e11bd5ac8b49bbe
povrazor <povrazor@27a7268d-3c14-4fb0-ae7b-d745656660af>
2013-04-27 23:30:58 +0000

    Not actually movement, but he moves, and the camera follows. Testing full camera bounds.

--- trunk/toom/toom.js
+++ trunk/toom/toom.js
@@ -1,8 +1,42 @@
 // - ------------------------------------------------------------------------------------------ - //
+var Room;
+// - ------------------------------------------------------------------------------------------ - //
+function cRoom() {
+	this.Base = new Vector2D(0,78);
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+
+// - ------------------------------------------------------------------------------------------ - //
+var Player;
+// - ------------------------------------------------------------------------------------------ - //
+function cPlayer() {
+	this.Pos = new Vector2D(0,0);
+	this.TargetPos = new Vector2D(0,0);
+}
+// - ------------------------------------------------------------------------------------------ - //
+cPlayer.prototype.Step = function() {
+	this.Pos = Sub(this.Pos,MultScalar(Sub(this.Pos,this.TargetPos),0.1));
+}
+// - ------------------------------------------------------------------------------------------ - //
+cPlayer.prototype.GetPos = function() {
+	return new Vector2D( -FCamera.x+this.Pos.x+Room.Base.x, -FCamera.y+this.Pos.y+Room.Base.y );
+}	
+// - ------------------------------------------------------------------------------------------ - //
+cPlayer.prototype.Draw = function() {
+	var Pos = this.GetPos();
+	gfxDraw( Art.Man, Pos.x, Pos.y, 4 );//(Stepper>>3)&3 );	
+}
+// - ------------------------------------------------------------------------------------------ - //
+
+// - ------------------------------------------------------------------------------------------ - //
 var Music;
 // - ------------------------------------------------------------------------------------------ - //
 function Init() {
 	Music = sndLooped("Music");	
+	
+	Room = new cRoom();
+	Player = new cPlayer();
 }
 // - ------------------------------------------------------------------------------------------ - //
 function GainFocus() {
@@ -19,14 +53,33 @@ var Stepper = 0;
 function Step() {
 	Stepper++;
 	
+	// Mouse Cursor Part //
 	var TargetX = ((Mouse.x-BaseX)/BaseX)*32.0;
 	var TargetY = ((Mouse.y-BaseY)/BaseY)*32.0;
+
+	// Player Position Part //
+	var Limit = 1010; // 1010 wide, 54 pixels inward //
+	TargetX += (Player.Pos.x / Limit) * 90.0; 
 	
-	CameraX -= (CameraX-TargetX)*0.1;
-	CameraY -= (CameraY-TargetY)*0.1;
+	Camera.x -= (Camera.x-TargetX)*0.1;
+	Camera.y -= (Camera.y-TargetY)*0.1;
 	
-	FCameraX = Math.floor(CameraX);
-	FCameraY = Math.floor(CameraY);
+	FCamera.x = Math.floor(Camera.x);
+	FCamera.y = Math.floor(Camera.y);
+	
+	if ( Input_MouseBits ) {
+		Player.TargetPos.x = (Mouse.x+Camera.x-BaseX);
+
+		var HalfLimit = (Limit>>1);
+
+		// Constrain Player Pos //
+		if ( Player.TargetPos.x < -(HalfLimit-54-16) )
+			Player.TargetPos.x = -(HalfLimit-54-16);
+		if ( Player.TargetPos.x > (HalfLimit-54) )
+			Player.TargetPos.x = (HalfLimit-54);
+	}
+	
+	Player.Step();
 }
 // - ------------------------------------------------------------------------------------------ - //
 
@@ -34,11 +87,11 @@ function Step() {
 function Draw() {
 //	gfxClear( RGB(0,0,0) );
 
-	BX = BaseX-FCameraX;
-	BY = BaseY-FCameraY;
+	BX = BaseX-FCamera.x;
+	BY = BaseY-FCamera.y;
 	
 	gfxDrawLayer( BGLayer );
-	gfxDraw( Art.Man, -FCameraX+0, -FCameraY+78, 4 );//(Stepper>>3)&3 );
+	Player.Draw();
 	gfxDrawLayer( FGLayer );
 	
 	if ( Mouse.Visible ) {
@@ -46,18 +99,20 @@ function Draw() {
 		ctx.fillRect( Mouse.x-10,Mouse.y-10,20,20 );
 	}
 	
+	var PlayerPos = Player.GetPos();
+	
 	ctx.fillStyle = RGB(255,255,255);
 	ctx.font = '20px Pixel';
 	var Text = 'Drek,';
 	var TD = ctx.measureText(Text);
-	ctx.fillText(Text, BX-(TD.width>>1), BY-78+48-20);
+	ctx.fillText(Text, BaseX+PlayerPos.x-(TD.width>>1), BaseY+PlayerPos.y-96-20);
 
 	Text = "Why are you ignoring me?";
 	TD = ctx.measureText(Text);
 	if ( (Stepper >> 5)&1 ) {
 		Text = "Why are you ignoring me?_";
 	}
-	ctx.fillText(Text, BX-(TD.width>>1), BY-78+48);
+	ctx.fillText(Text, BaseX+PlayerPos.x-(TD.width>>1), BaseY+PlayerPos.y-96);
 
 }
 // - ------------------------------------------------------------------------------------------ - //
